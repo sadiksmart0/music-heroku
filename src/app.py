c9036c6a8c9b56a14108affa8ea09cd5f9bc0cae
A-M-MUKTAR <sadiksmart0@gmail.com>
2023-02-02 19:10:02 +0100

    nltk

--- src/app.py
+++ src/app.py
@@ -7,72 +7,73 @@ import json
 import pandas as pd
 from nltk_utils import utils
 
-utils()
-#================ Gif loader ===================#
-file_ = open("images/prof.gif", "rb")
-contents = file_.read()
-data_url = base64.b64encode(contents).decode("utf-8")
-file_.close()
+    #================ Gif loader ===================#
+    file_ = open("images/prof.gif", "rb")
+    contents = file_.read()
+    data_url = base64.b64encode(contents).decode("utf-8")
+    file_.close()
 
-#================ Side Bar ===================#
-add_selectbox = st.sidebar.selectbox(
-    "Explore our top 10",
-    ("Happy", "Sad", "Angry","Relaxed")
-)
-
-
-
-
-#================ App Header ===================#
-head, photo = st.columns(2)    
-with head:   
-    st.title("With music there is no tension.")
-
-with photo:
-    st.markdown(f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">', unsafe_allow_html=True,)
-
-
-
-#========================   RECOMMMENDATION VIEW  ==================================#
-def view(result):
-    img_col, play_col = st.columns(2)
-    for song in result:
-        with img_col:
-            st.subheader(song["title"])
-            st.write(song["artist"]["name"])
-            st.image(song["artist"]["picture"])
-        with play_col:
-            st.subheader(song["album"]["title"])
-            st.write(f'Duration: {round(song["duration"]/60,2)} min')
-            st.markdown(f"[![Foo](https://cdn-icons-png.flaticon.com/128/9458/9458362.png)]({song['link']})")
+    #================ Side Bar ===================#
+    add_selectbox = st.sidebar.selectbox(
+        "Explore our top 10",
+        ("Happy", "Sad", "Angry","Relaxed")
+    )
 
 
 
 
+    #================ App Header ===================#
+    head, photo = st.columns(2)    
+    with head:   
+        st.title("With music there is no tension.")
 
-st.subheader("Search by Lyrics")
-txt = st.text_area('Insert Song Lyrics', '''
-It was the best of times, it was the worst of times, it was
-the age of wisdom, it was the age of foolishness, it was
-the epoch of belief, it was the epoch of incredulity, it
-was the season of Light, it was the season of Darkness, it
-was the spring of hope, it was the winter of despair, (...)
-''')
-
-lyrics_data = {
-    'lyrics_id': str(uuid.uuid1()),
-    'timestamp': datetime.datetime.now(),
-    'lyrics': txt
-}
-
-if st.button('Submit'):
-    mood = recommend_with_lyrics(txt)
-    mood = json.loads(mood["mood"])
-    result = pd.DataFrame(mood)
-    result = final_recommended(result)
-    view(result)
+    with photo:
+        st.markdown(f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">', unsafe_allow_html=True,)
 
 
+
+    #========================   RECOMMMENDATION VIEW  ==================================#
+    def view(result):
+        img_col, play_col = st.columns(2)
+        for song in result:
+            with img_col:
+                st.subheader(song["title"])
+                st.write(song["artist"]["name"])
+                st.image(song["artist"]["picture"])
+            with play_col:
+                st.subheader(song["album"]["title"])
+                st.write(f'Duration: {round(song["duration"]/60,2)} min')
+                st.markdown(f"[![Foo](https://cdn-icons-png.flaticon.com/128/9458/9458362.png)]({song['link']})")
+
+
+
+
+
+    st.subheader("Search by Lyrics")
+    txt = st.text_area('Insert Song Lyrics', '''
+    It was the best of times, it was the worst of times, it was
+    the age of wisdom, it was the age of foolishness, it was
+    the epoch of belief, it was the epoch of incredulity, it
+    was the season of Light, it was the season of Darkness, it
+    was the spring of hope, it was the winter of despair, (...)
+    ''')
+
+    lyrics_data = {
+        'lyrics_id': str(uuid.uuid1()),
+        'timestamp': datetime.datetime.now(),
+        'lyrics': txt
+    }
+
+    if st.button('Submit'):
+        mood = recommend_with_lyrics(txt)
+        mood = json.loads(mood["mood"])
+        result = pd.DataFrame(mood)
+        result = final_recommended(result)
+        view(result)
+
+if __name__ == __main__:
+    utils()
+    main()
 
 
 

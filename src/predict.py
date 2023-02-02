ecd71ec46ae37dda5e06281a7845120c64644472
A-M-MUKTAR <sadiksmart0@gmail.com>
2023-02-02 19:54:20 +0100

    nltk

--- src/predict.py
+++ src/predict.py
@@ -28,7 +28,7 @@ def tokenize_lyrics(text):
 #================================ PREDICT WITH LYRICS ============================#
 def recommend_with_lyrics(text):
     song = tokenize_lyrics(text)
-    response = requests.post("http://127.0.0.1:8000/lyrics", json={"text": song})
+    response = requests.post("http://0.0.0.0:8080/lyrics", json={"text": song})
     response = json.loads(response.text)
     return  response
 
